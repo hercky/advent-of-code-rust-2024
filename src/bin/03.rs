576dba85c218787d076cba00c626152a0ebde3e0
hercky <hercs19@gmail.com>
2024-12-04 13:24:37 -0500

    day 3 - part 2

--- src/bin/03.rs
+++ src/bin/03.rs
@@ -3,12 +3,12 @@ advent_of_code::solution!(3);
 use regex::Regex;
 
 pub fn part_one(input: &str) -> Option<u32> {
-    let re = Regex::new(r"mul\((\d+),(\d+)\)").unwrap();
+    let re = Regex::new(r"mul\((?<a>\d+),(?<b>\d+)\)").unwrap();
     let mut sum = 0;
 
     for cap in re.captures_iter(input) {
-        let a: u32 = cap[1].parse().unwrap();
-        let b: u32 = cap[2].parse().unwrap();
+        let a: u32 = cap["a"].parse().unwrap();
+        let b: u32 = cap["b"].parse().unwrap();
         sum += a * b;
     }
 
@@ -16,7 +16,30 @@ pub fn part_one(input: &str) -> Option<u32> {
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
-    None
+    let re_combined =
+        Regex::new(r"(?<do>do\(\))|(?<dont>don't\(\))|mul\((?<a>\d+),(?<b>\d+)\)").unwrap();
+
+    let mut sum = 0;
+    let mut skip = false;
+
+    for cap in re_combined.captures_iter(input) {
+        if cap.name("do").is_some() {
+            // do()
+            skip = false;
+        } else if cap.name("dont").is_some() {
+            // don't()
+            skip = true;
+        } else {
+            // mul(a,b)
+            let a: u32 = cap["a"].parse().unwrap();
+            let b: u32 = cap["b"].parse().unwrap();
+            if !skip {
+                sum += a * b;
+            }
+        }
+    }
+
+    Some(sum)
 }
 
 #[cfg(test)]
@@ -32,6 +55,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(48));
     }
 }
