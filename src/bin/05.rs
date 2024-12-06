260c4ff3188744d54d8d9d8cf33a6fa4f7e41f26
hercky <hercs19@gmail.com>
2024-12-06 14:23:17 -0500

    day 5 part 2

--- src/bin/05.rs
+++ src/bin/05.rs
@@ -139,7 +139,89 @@ pub fn part_one(input: &str) -> Option<u32> {
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
-    None
+    let mut node_to_id = HashMap::new();
+    let mut id_to_node = HashMap::new();
+    let mut edges = Vec::new();
+
+    let mut edge_scan = true;
+
+    let mut orders: Vec<Vec<u32>> = Vec::new();
+
+    for line in input.lines() {
+        if line.trim().is_empty() {
+            edge_scan = false;
+            continue;
+        }
+
+        if edge_scan {
+            let (left, right) = line.trim().split_once('|').unwrap();
+
+            let left = left.parse::<u32>().unwrap();
+            let right = right.parse::<u32>().unwrap();
+
+            if !node_to_id.contains_key(&left) {
+                node_to_id.insert(left, node_to_id.len());
+                id_to_node.insert(node_to_id.len() - 1, left);
+            }
+            if !node_to_id.contains_key(&right) {
+                node_to_id.insert(right, node_to_id.len());
+                id_to_node.insert(node_to_id.len() - 1, right);
+            }
+            // add the edge
+            edges.push((node_to_id[&left], node_to_id[&right]));
+        } else {
+            orders.push(
+                line.trim()
+                    .split(',')
+                    .map(|s| s.parse::<u32>().unwrap())
+                    .collect(),
+            );
+        }
+    }
+
+    let n = node_to_id.len();
+    let mut adjacency_matrix: Vec<Vec<bool>> = vec![vec![false; n]; n];
+
+    for (left, right) in edges {
+        adjacency_matrix[left][right] = true;
+    }
+
+    // bubble sort on the sub adjacency matrix
+
+    let mut answer = 0;
+
+    for order in orders {
+        let nodes_in_order = order.iter().map(|x| node_to_id[&x]).collect::<Vec<_>>();
+        let paths = floyd_warshall(&adjacency_matrix, &nodes_in_order);
+
+        let mut legit = true;
+
+        let nodes_in_order = order.iter().map(|x| node_to_id[&x]).collect::<Vec<_>>();
+        let paths = floyd_warshall(&adjacency_matrix, &nodes_in_order);
+
+        for i in 0..order.len() {
+            for j in 0..i {
+                if paths[node_to_id[&order[i]]][node_to_id[&order[j]]] {
+                    legit = false;
+                    break;
+                }
+            }
+            if !legit {
+                break;
+            }
+        }
+        if !legit {
+            let mut sorted_order = order.clone();
+            // sort the nodes in order based on the preference given by the paths matrix
+            sorted_order.sort_by(|a, b| {
+                paths[node_to_id[a]][node_to_id[b]].cmp(&paths[node_to_id[b]][node_to_id[a]])
+            });
+            // println!("{:?}", sorted_order);
+            answer += sorted_order[sorted_order.len() / 2];
+        }
+    }
+
+    Some(answer)
 }
 
 #[cfg(test)]
@@ -155,6 +237,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(123));
     }
 }
