2e1ef466028229a8092c877b5f3f240cce06de7e
hercky <hercs19@gmail.com>
2024-12-14 22:39:48 -0800

    day 7 - part 2

--- src/bin/07.rs
+++ src/bin/07.rs
@@ -43,7 +43,47 @@ pub fn part_one(input: &str) -> Option<u64> {
 }
 
 pub fn part_two(input: &str) -> Option<u64> {
-    None
+    let mut ans = 0;
+
+    for line in input.lines() {
+        let line = line.trim();
+        let (left, right) = line.split_once(':').unwrap();
+        let left = left.parse::<u64>().unwrap();
+        let right = right
+            .split_whitespace()
+            .map(|s| s.parse::<u64>().unwrap())
+            .collect::<Vec<u64>>();
+
+        let mut dp: Vec<u64> = Vec::new();
+
+        dp.push(right[0]);
+
+        for i in 1..right.len() {
+            let candidate = right[i];
+            let mut new_dp: Vec<u64> = Vec::new();
+
+            for j in 0..dp.len() {
+                let prev = dp[j];
+                // *
+                new_dp.push(prev * candidate);
+                // +
+                new_dp.push(prev + candidate);
+                // ||
+                new_dp.push(prev * 10u64.pow(candidate.ilog10() + 1) + candidate);
+            }
+
+            dp = new_dp;
+        }
+
+        for j in 0..dp.len() {
+            if dp[j] == left {
+                ans += left;
+                break;
+            }
+        }
+    }
+
+    Some(ans)
 }
 
 #[cfg(test)]
@@ -59,6 +99,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(11387));
     }
 }
