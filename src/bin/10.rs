36710ed9c2e7c3386353a339d2b452caeada1a49
hercky <hercs19@gmail.com>
2024-12-18 15:56:50 -0500

    day 10 - part 2

--- src/bin/10.rs
+++ src/bin/10.rs
@@ -97,8 +97,62 @@ pub fn part_one(input: &str) -> Option<u32> {
     Some(ans)
 }
 
+fn calculate_unique_paths_via_bfs(grid: &Vec<Vec<i32>>, i: usize, j: usize) -> Vec<Vec<u32>> {
+    let m = grid.len();
+    let n = grid[0].len();
+
+    let mut visited: Vec<Vec<u32>> = vec![vec![0; n]; m];
+
+    let mut queue = Vec::new();
+    queue.push((i, j));
+
+    while !queue.is_empty() {
+        let (i, j) = queue.remove(0);
+
+        visited[i][j] = visited[i][j] + 1;
+
+        for (ni, nj) in get_neighbors(grid, i, j) {
+            queue.push((ni, nj));
+        }
+    }
+
+    visited
+}
+
+fn count_unique_paths(grid: &Vec<Vec<i32>>, visited: &Vec<Vec<u32>>) -> u32 {
+    let m = grid.len();
+    let n = grid[0].len();
+
+    let mut ans: u32 = 0;
+
+    for i in 0..m {
+        for j in 0..n {
+            if visited[i][j] > 0 && grid[i][j] == 9 {
+                ans += visited[i][j];
+            }
+        }
+    }
+
+    ans
+}
+
 pub fn part_two(input: &str) -> Option<u32> {
-    None
+    let grid = parse_input(input);
+    let m = grid.len();
+    let n = grid[0].len();
+
+    let mut ans: u32 = 0;
+
+    for i in 0..m {
+        for j in 0..n {
+            if grid[i][j] == 0 {
+                let visited: Vec<Vec<u32>> = calculate_unique_paths_via_bfs(&grid, i, j);
+                ans += count_unique_paths(&grid, &visited);
+            }
+        }
+    }
+
+    Some(ans)
 }
 
 #[cfg(test)]
@@ -114,6 +168,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(81));
     }
 }
