a7886a2fa28e001d9ce7e42468478914a4dbb0dc
hercky <hercs19@gmail.com>
2024-12-19 17:18:32 -0500

    day 11 - part 2

--- src/bin/11.rs
+++ src/bin/11.rs
@@ -43,41 +43,41 @@ fn fast_blink(stones: &HashMap<u64, u64>) -> HashMap<u64, u64> {
     let mut new_stones = HashMap::new();
 
     for (&stone, &count) in stones {
-        if stone == 0 {
-            *new_stones.entry(1).or_insert(0) += count;
-        } else if stone.to_string().len() % 2 == 0 {
-            let num_str = stone.to_string();
-            let (left, right) = num_str.split_at(num_str.len() / 2);
-            let left_num = left.parse::<u64>().unwrap();
-            let right_num = right.parse::<u64>().unwrap();
-            *new_stones.entry(left_num).or_insert(0) += count;
-            *new_stones.entry(right_num).or_insert(0) += count;
-        } else {
-            *new_stones.entry(stone * 2024).or_insert(0) += count;
+        let next_stone = match stone {
+            0 => vec![1],
+            _ if stone.to_string().len() % 2 == 0 => {
+                let num_str = stone.to_string();
+                let (left, right) = num_str.split_at(num_str.len() / 2);
+                let left_num = left.parse::<u64>().unwrap();
+                let right_num = right.parse::<u64>().unwrap();
+                vec![left_num, right_num]
+            }
+            _ => vec![stone * 2024],
+        };
+
+        for stone in next_stone {
+            *new_stones.entry(stone).or_insert(0) += count;
         }
     }
 
     new_stones
 }
 
-pub fn part_two(input: &str) -> Option<u32> {
+pub fn part_two(input: &str) -> Option<u64> {
     let mut stones = HashMap::new();
     for stone in parse_input(input) {
         *stones.entry(stone).or_insert(0) += 1;
     }
 
-    for i in 0..5 {
+    println!("{:?}", stones);
+
+    for _ in 0..75 {
         stones = fast_blink(&stones);
     }
 
-    println!("{:?}", stones);
+    let total = stones.values().sum::<u64>();
 
-    let mut total = 0;
-    for (&stone, &count) in &stones {
-        total += count;
-    }
-
-    Some(total as u32)
+    Some(total)
 }
 
 #[cfg(test)]
@@ -93,6 +93,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(239321955280205));
     }
 }
