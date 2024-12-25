d6237a4e21eb73f5ecf42ed3c656128925129248
hercky <hercs19@gmail.com>
2024-12-25 18:27:08 -0500

    day 15 - part 1.1

--- src/bin/15.rs
+++ src/bin/15.rs
@@ -136,8 +136,136 @@ pub fn part_one(input: &str) -> Option<u64> {
     Some(sum)
 }
 
-pub fn part_two(input: &str) -> Option<u32> {
-    None
+#[derive(Debug, Clone, Copy)]
+enum BlockType {
+    Empty,
+    Wall,
+    Box,
+    Robot,
+}
+
+impl std::fmt::Display for BlockType {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            BlockType::Empty => write!(f, ".."),
+            BlockType::Wall => write!(f, "##"),
+            BlockType::Box => write!(f, "[]"),
+            BlockType::Robot => write!(f, "@."),
+        }
+    }
+}
+
+#[derive(Debug, Clone, Copy)]
+struct Block {
+    x_left: u32,
+    x_right: u32,
+    y: u32,
+    block_type: BlockType,
+}
+
+impl Block {
+    fn new(x: u32, y: u32, block_type: BlockType) -> Self {
+        Block {
+            x_left: x,
+            x_right: x + 1,
+            y,
+            block_type,
+        }
+    }
+}
+
+fn print_block_grid(grid: &Vec<Vec<Block>>) {
+    for row in grid {
+        for block in row {
+            print!("{}", block.block_type.to_string());
+        }
+        println!();
+    }
+}
+
+// fn next_position(direction: char, current_block: Block, grid: &Vec<Vec<char>>) -> (u32, u32, u32) {
+//     let (mut x_left, mut x_right, mut y) = (current_block.x_left, current_block.x_right, current_block.y);
+
+//     match direction {
+//         '>' => {
+//             x_right += 2;
+//             x_left += 2;
+//         }
+//         '<' => {
+//             x_right -= 2;
+//             x_left -= 2;
+//         }
+//         'v' => y += 1,
+//         '^' => y -= 1,
+//     }
+
+//     if grid[x_left][y] == '#' || grid[x_right][y] == '#' {
+//         return (x_left, x_right, y, BlockType::Wall);
+//     } else if grid[x_left][y] == '.' || grid[x_right][y] == '.' {
+//         return (x_left, x_right, y, BlockType::Empty);
+//     } else if grid[x_left][y] == ['[', ']'] || grid[x_right][y] == ['[', ']'] {
+//         return (x_left, x_right, y, BlockType::Box);
+//     }
+
+//     (x_left, x_right, y)
+// }
+
+// fn push_block(block: Block, direction: char, grid: &Vec<Vec<char>>) {
+//     let (mut x_left, mut x_right, mut y) = (block.x_left, block.x_right, block.y);
+
+//     let next_position
+
+//     if next_position(direction, x_left, x_right, y) == '.' {
+//         // make the change for this block
+//         // move this block
+//     } else if next_position(direction, x_left, x_right, y) == ['[', ']'] {
+//         // push that block in the direction
+//         push_block(
+//             next_position(direction, x_left, x_right, y),
+//             direction,
+//             grid,
+//         );
+//     } else {
+//         // '#'
+//         // dead end, can't move
+//     }
+// }
+
+pub fn part_two(input: &str) -> Option<u64> {
+    let (grid, path) = parse_input(input);
+
+    // convert the grid to a block grid
+    let mut block_grid = Vec::new();
+    for i in 0..grid.len() {
+        let mut row = Vec::new();
+        for j in 0..grid[i].len() {
+            match grid[i][j] {
+                '.' => row.push(Block::new(2 * i as u32, j as u32, BlockType::Empty)),
+                '#' => row.push(Block::new(2 * i as u32, j as u32, BlockType::Wall)),
+                'O' => row.push(Block::new(2 * i as u32, j as u32, BlockType::Box)),
+                '@' => row.push(Block::new(2 * i as u32, j as u32, BlockType::Robot)),
+                '\n' => continue,
+                _ => panic!("Invalid character in grid"),
+            }
+        }
+        block_grid.push(row);
+    }
+
+    print_block_grid(&block_grid);
+
+    return None;
+
+    let mut sum = 0;
+
+    for i in 0..grid.len() {
+        for j in 0..grid[i].len() {
+            if grid[i][j] == 'O' {
+                sum += i as u64 * 100 + j as u64;
+            }
+        }
+    }
+
+    Some(sum)
 }
 
 #[cfg(test)]
