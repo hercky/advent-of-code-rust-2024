dcd8a3b90112c7ac50c5555a131d68f5d416cc6c
hercky <hercs19@gmail.com>
2025-01-02 19:09:51 -0500

    day 22 - part 2

--- src/bin/22.rs
+++ src/bin/22.rs
@@ -1,5 +1,7 @@
 advent_of_code::solution!(22);
 
+use std::collections::{HashMap, HashSet};
+
 // Hint from reddit: The problem is based on the linear feedback shift register
 // https://en.wikipedia.org/wiki/Linear_feedback_shift_register
 /// [Xorshift LFSR](https://en.wikipedia.org/wiki/Linear-feedback_shift_register#Xorshift_LFSRs).
@@ -36,7 +38,32 @@ pub fn part_one(input: &str) -> Option<usize> {
 }
 
 pub fn part_two(input: &str) -> Option<u64> {
-    None
+    let numbers: Vec<usize> = input.lines().map(|line| line.parse().unwrap()).collect();
+
+    let mut total_bananas: HashMap<Vec<i32>, u64> = HashMap::new();
+
+    for &num in &numbers {
+        let mut seen = HashSet::new();
+        let mut diffs = Vec::new();
+        let mut current = num;
+
+        for i in 0..2000 {
+            let next = hash(current);
+            diffs.push(((next % 10) as i32) - ((current % 10) as i32));
+            current = next;
+
+            if i >= 3 {
+                let sequence = diffs.clone();
+                if !seen.contains(&sequence) {
+                    seen.insert(sequence.clone());
+                    *total_bananas.entry(sequence).or_default() += (current % 10) as u64;
+                }
+                diffs.remove(0);
+            }
+        }
+    }
+
+    total_bananas.values().max().copied()
 }
 
 #[cfg(test)]
@@ -52,6 +79,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(24));
     }
 }
